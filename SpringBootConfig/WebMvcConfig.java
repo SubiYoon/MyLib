db5e247093f380b1b7dce43ae3b64ce34cf0aeba
YoonDongSub <yoondongsub92@gmail.com>
2023-02-13 01:53:25 +0900

    2023.02.13

---
+++ SpringBootConfig/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.camp.camping;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    //property에 명시된 경로 데이터값
+    @Value("${imagesdir}")
+    String imagesdir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:" + imagesdir); //리눅스 root에서 시작하는 폴더 경로
+    }
+}
