0ffeb1af6eb05550a4b06cb43f9c75725b1beb84
ABCD <ABCtoDev@gmail.com>
2025-01-20 18:42:05 +0900

    lib edit

---
+++ src/main/java/my/lib/list/extLinkedList/listNode/Node.java
@@ -0,0 +1,16 @@
+package my.lib.list.extLinkedList.listNode;
+
+public class Node<T> {
+    public T item;
+    public Node<T> next;
+
+    public Node(T newItem) {
+        this.item = newItem;
+        this.next = null;
+    }
+
+    public Node(T newItem, Node next) {
+        this.item = newItem;
+        this.next = next;
+    }
+}
